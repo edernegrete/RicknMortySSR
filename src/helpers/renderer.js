c0f868e76cb501de22762cde6db75a02b9575b6f
Eder Negrete <ederyirnglz@gmail.com>
2018-12-10 17:26:57 -0600

    fix(renderer): added meta viewport

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -24,6 +24,7 @@ export default (req, store) => {
     return `
       <html>
         <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
           <link rel="stylesheet" href="/css/main.css">  
         </head>
